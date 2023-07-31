6d3158cfdcd7ee15fde18d5b7a50499d8d8fb041
MichaelPNair <michael.p.nair@gmail.com>
2023-07-31 22:59:28 +1000

    add function to remove losers from entered array

--- src/VsScreen.jsx
+++ src/VsScreen.jsx
@@ -1,13 +1,21 @@
 import FiveGifs from "./FiveGifs"
 
-export default function VsScreen({searchTerm1, searchTerm2}) {
+export default function VsScreen({searchTerm1, searchTerm2, removeElement}) {
+
+    function handleButton1(){
+        removeElement(1)
+    }
+
+    function handleButton2(){
+        removeElement(0)
+    }
     
 
     return (
         <div>
             <h2>{searchTerm1 + ' vs ' + searchTerm2}</h2>
-            <button>{searchTerm1} are the best!</button>
-            <button>{searchTerm2} are better!</button>
+            <button onClick={handleButton1}>{searchTerm1} are the best!</button>
+            <button onClick={handleButton2}>{searchTerm2} are better!</button>
             <FiveGifs searchTerm={searchTerm1}/>
             <FiveGifs searchTerm={searchTerm2}/>
         </div>
