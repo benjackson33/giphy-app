10cc590f0b07219b6fd1f4ed97d117bacc09c555
Ben Jackson <benjacksonsmac@gmail.com>
2023-07-31 17:21:14 +1200

    initial commit

---
+++ src/VictoryScreen.jsx
@@ -0,0 +1,6 @@
+export default function VictoryScreen() {
+    return (
+        <div>
+        </div>
+    )
+}
---
+++ src/VsScreen.jsx
@@ -0,0 +1,9 @@
+import FiveGifs from "./FiveGifs"
+
+export default function VsScreen() {
+    return (
+        <div>
+            <FiveGifs />
+        </div>
+    )
+}
